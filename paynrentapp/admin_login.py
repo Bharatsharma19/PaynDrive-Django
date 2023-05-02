f26a5269bb99268718c2f555b2a4f5db178bf71b
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2023-05-02 05:35:34 +0530

    Updated

--- paynrentapp/admin_login.py
+++ paynrentapp/admin_login.py
@@ -20,11 +20,9 @@ def CheckAdminLogin(request):
             q = "select * from  paynrentapp_administrator  where (mobileno='{0}' or emailid='{0}') and password='{1}'".format(
                 request.GET['mobileno'], request.GET['password'])
 
-            print(q)
             cursor = connection.cursor()
             cursor.execute(q)
             record = tuple_to_dict.ParseDictMultipleRecord(cursor)
-            print("Check", record)
 
             if(len(record) == 0):
                 return render(request, "AdminLogin.html", {'message': "Inavlid Adminid/Password"})
